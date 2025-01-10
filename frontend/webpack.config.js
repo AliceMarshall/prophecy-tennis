e1f8dbeae01fd2588fb8cd141bdae364d67ca5cb
Alice Marshall <marshall.alice92@gmail.com>
2025-01-10 16:15:25 +0000

    Updates to webpack/app setup

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,14 +1,22 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable no-undef */
 const prod = process.env.NODE_ENV === 'production';
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+require('dotenv').config({ path: prod ? './.env' : './.env.development' });
 
 module.exports = {
     mode: prod ? 'production' : 'development',
     entry: './src/index.tsx',
     output: {
-        path: __dirname + '/build/',
+        path: path.resolve(__dirname, 'build'),
+        filename: 'index_bundle.js',
+        publicPath: '/',
     },
     module: {
         rules: [
@@ -27,9 +35,17 @@ module.exports = {
         ],
     },
     resolve: {
-        fallback: { buffer: false },
+        fallback: {
+            buffer: false,
+            crypto: require.resolve('crypto-browserify'),
+            vm: require.resolve('vm-browserify'),
+            stream: require.resolve('stream-browserify'),
+        },
     },
     devtool: prod ? undefined : 'source-map',
+    devServer: {
+        historyApiFallback: true,
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: 'index.html',
@@ -39,5 +55,8 @@ module.exports = {
             patterns: [{ from: 'src/assets/images', to: 'assets' }],
         }),
         new MiniCssExtractPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': JSON.stringify(process.env),
+        }),
     ],
 };
